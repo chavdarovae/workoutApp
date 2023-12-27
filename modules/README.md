0d27bf5b8ec785843de1af76cc785a97787fa001
Emiliya Chavdarova <chavarovae>
2023-12-27 15:43:50 +0100

    Migrate to nx

--- modules/README.md
+++
@@ -1,7 +0,0 @@
-# workout
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test workout` to execute the unit tests.
--- modules/src/lib/lib.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Route } from '@angular/router';
-import { WorkoutComponent } from './workout/workout.component';
-
-export const workoutRoutes: Route[] = [
-	{ path: '', component: WorkoutComponent },
-];
